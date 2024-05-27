594374f724d54ffb3fd2ebfffd3f54869e9490cf
Aatti13 <86865554+Aatti13@users.noreply.github.com>
2024-05-27 21:51:33 +0530

    question on Linear Regression

---
+++ Questions/linear_reg_1.py
@@ -0,0 +1,28 @@
+# CSV Dataset --> Take from codebasics.io --> 
+
+try:
+  import pandas as pd
+  from sklearn.linear_model import LinearRegression
+  import matplotlib.pyplot as mplt
+except ImportError as e:
+  print(e.msg)
+
+df = pd.read_csv('canada_per_capita_income.csv')
+
+X = df[['year']]
+y = df['per capita income (US$)']
+
+model = LinearRegression()
+model.fit(X, y)
+
+yPred = model.predict(X)
+
+print(model.coef_)
+print(model.intercept_)
+print(model.predict([[2020]]))
+
+mplt.scatter(X, y, marker='+', color="red")
+mplt.plot(df[['year']], yPred, color="blue")
+mplt.xlabel("Year")
+mplt.ylabel("Per Capita Income (US$)")
+mplt.show()
