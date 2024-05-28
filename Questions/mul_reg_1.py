e0cfc27804894df425c0c5fef03e3c82f2e85855
Aatti13 <86865554+Aatti13@users.noreply.github.com>
2024-05-28 14:47:52 +0530

    1

--- Questions/mul_reg_1.py
+++ Questions/mul_reg_1.py
@@ -1,6 +1,25 @@
 try:
-  import numpy as np
   import pandas as pd
+  from sklearn.linear_model import LinearRegression
 except ImportError as e:
   print(e.msg)
 
+df = pd.read_csv('homeprices.csv')
+
+# Data Pre-processing
+df[['bedrooms']] = df[['bedrooms']].fillna(float(df.iloc[0].bedrooms.mean()))
+
+X1 = df[['area']]
+X2 = df[['bedrooms']]
+X3 = df[['age']]
+
+
+y = df['price']
+
+X = df.drop('price', axis='columns')
+
+lrModel = LinearRegression()
+lrModel.fit(X, y)
+
+print(lrModel.predict([[3000, 3, 40]]))
+print(lrModel.predict([[2500, 4, 5]]))
